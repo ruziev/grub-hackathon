c6d8c8058a4cb4c35724c831217e7af82c65e585
Ruziev <jjruziev@gmail.com>
2017-04-22 23:19:52 +0300

    Linked Main Storyboard

---
+++ RGUB-client/RGUB-client/MyProfileVC.swift
@@ -0,0 +1,13 @@
+//
+//  MyProfileVC.swift
+//  RGUB-client
+//
+//  Created by Vladimir Pavlov on 4/22/17.
+//  Copyright © 2017 Ruziev. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileVC: UIViewController {
+
+}
