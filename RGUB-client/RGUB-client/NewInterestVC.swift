ac5d1f4d7e442a882240f2090894e68fd9044ab5
Vladimir Pavlov <nofake21rus@gmail.com>
2017-04-23 12:19:50 +0300

    Added NewInterestVC
    Also renamed Main storyboard to MyProfile

---
+++ RGUB-client/RGUB-client/NewInterestVC.swift
@@ -0,0 +1,13 @@
+//
+//  NewInterestVC.swift
+//  RGUB-client
+//
+//  Created by Vladimir Pavlov on 4/23/17.
+//  Copyright © 2017 Ruziev. All rights reserved.
+//
+
+import UIKit
+
+class NewInterestVC: UIViewController {
+
+}
